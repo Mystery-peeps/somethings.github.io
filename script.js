d377650002532f26f8a8c6c06650f0f49620ef5a
Mystery-peeps <rwigemafamily@gmail.com>
2025-12-25 14:50:46 -0700

    Correct syntax errors in jump and checkDead functions
    Fix multiple syntax errors and improve jump logic.

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
 var character = ducument.getElementById("character");
 var block = ducument.getElementById("block");
 function jump(){
+  if(character.classList !="animate"){
   character.classList.add("animate";)
-  setTimeout(
+  }
+  setTimeout(function(){
+      character.classList.remove("animate";)
+  }, 500);
 }
+
+var checkDead = setInterval(function(){
+ var characterTop = 
+   parsInt(window.getCumputedStyle(character).getPropertyValue("top"));
+   var blockLeft = 
+   parsInt(window.getCumputedStyle(block).getPropertyValue("left"));
+  If (blockLeft<20 && blockLeft>0 %% characterTop>=130){
+
+    block.style.animation = "none";
+     block.style.display = "none";
+    alert("U Lost");
+  }
+},10);
