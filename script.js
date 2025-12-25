5f73dfbac13d90b92fab00b38248b5ca990980a4
Algfish31 <148805525+Algfish31@users.noreply.github.com>
2025-12-25 15:03:27 -0700

    petroleum is bad at coding js
    Fixed typos and formatting issues in jump function and checkDead interval.

--- script.js
+++ script.js
@@ -1,23 +1,22 @@
 var character = ducument.getElementById("character");
 var block = ducument.getElementById("block");
-function jump(){
-  if(character.classList !="animate"){
-  character.classList.add("animate");
+function jump () {
+  if (character.classList != "animate") {
+    character.classList.add("animate");
   }
-  setTimeout(function(){
-      character.classList.remove("animate");
+  setTimeout(function () {
+    character.classList.remove("animate");
   }, 500);
 }
 
-var checkDead = setInterval(function(){
+var checkDead = setInterval(function () {
  var characterTop = 
    parsInt(window.getCumputedStyle(character).getPropertyValue("top"));
-   var blockLeft = 
-   parsInt(window.getCumputedStyle(block).getPropertyValue("left"));
+   var blockLeft = parsInt(window.getCumputedStyle(block).getPropertyValue("left"));
   If (blockLeft<20 && blockLeft>0 && characterTop>=130){
 
     block.style.animation = "none";
-     block.style.display = "none";
+    block.style.display = "none";
     alert("U Lost");
   }
 },10);
